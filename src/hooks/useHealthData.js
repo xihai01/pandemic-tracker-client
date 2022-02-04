b90b7dbaf9f0cd5a55a4b40abf00581892c69083
Xihai Luo <xihai.luo01@gmail.com>
2022-02-03 22:20:29 -0500

    set credentials to true for health stages CRUD operations

--- src/hooks/useHealthData.js
+++ src/hooks/useHealthData.js
@@ -1,63 +1,93 @@
-import { useReducer, useEffect } from 'react';
+import { useReducer, useEffect } from "react";
 import adminReducer, { SET_REGIONS } from "reducer/admin_reducer";
-import axios from 'axios'
+import axios from "axios";
 
-
-export default function useHealthData(){
+export default function useHealthData() {
   const [state, dispatch] = useReducer(adminReducer, {
     healthRegions: [],
     loading: true,
-    error: null
-  })
+    error: null,
+  });
 
-  useEffect(()=>{
-    axios.get(`/admin/health_regions`, { withCredentials: true })
-    .then((res)=>{
-      dispatch({type: SET_REGIONS, healthRegions: res.data});
-    })
-    .catch()
-  } ,[]);
-
+  useEffect(() => {
+    axios
+      .get(`/admin/health_regions`, { withCredentials: true })
+      .then((res) => {
+        dispatch({ type: SET_REGIONS, healthRegions: res.data });
+      })
+      .catch();
+  }, []);
 
   const columns = [
-    { title: 'Region code', field: 'region_code' },
-    { title: 'Name', field: 'region_name',filtering: false},
-    { title: 'Lockdown Stage', field: 'stage_id'},
-    { title: 'Created On', field: 'created_at',editable: 'never',filtering: false},
-    { title: 'Last Update', field: 'updated_at',editable: 'never',filtering: false}
-
-  ]
-
+    { title: "Region code", field: "region_code" },
+    { title: "Name", field: "region_name", filtering: false },
+    { title: "Lockdown Stage", field: "stage_id" },
+    {
+      title: "Created On",
+      field: "created_at",
+      editable: "never",
+      filtering: false,
+    },
+    {
+      title: "Last Update",
+      field: "updated_at",
+      editable: "never",
+      filtering: false,
+    },
+  ];
 
   function editRow(data) {
-    return Promise.all([axios.put(`/admin/health_regions/${data.id}`,{region_name:data.region_name,region_code:data.region_code,stage_id:data.stage_id}),axios.get(`/admin/health_regions`)])
-    .then(([res1,res2])=> {
-      console.log(`data edited sucessfully`);
-      dispatch({type: SET_REGIONS, healthRegions: res2.data});
-    })
-    .catch(()=> console.log(`error editing data in DB`));
-
+    return Promise.all([
+      axios.put(
+        `/admin/health_regions/${data.id}`,
+        {
+          region_name: data.region_name,
+          region_code: data.region_code,
+          stage_id: data.stage_id,
+        },
+        { withCredentials: true }
+      ),
+      axios.get(`/admin/health_regions`, { withCredentials: true }),
+    ])
+      .then(([res1, res2]) => {
+        console.log(`data edited sucessfully`);
+        dispatch({ type: SET_REGIONS, healthRegions: res2.data });
+      })
+      .catch(() => console.log(`error editing data in DB`));
   }
 
   function deleteRow(data) {
-    return Promise.all([axios.delete(`/admin/health_regions/${data.id}`),axios.get(`/admin/health_regions`)])
-    .then(([res1,res2])=> {
-      console.log(`data deleted sucessfully`);
-      dispatch({type: SET_REGIONS, healthRegions: res2.data});
-    })
-    .catch(()=> console.log(`error editing data in DB`));
-
-
+    return Promise.all([
+      axios.delete(`/admin/health_regions/${data.id}`, {
+        withCredentials: true,
+      }),
+      axios.get(`/admin/health_regions`, { withCredentials: true }),
+    ])
+      .then(([res1, res2]) => {
+        console.log(`data deleted sucessfully`);
+        dispatch({ type: SET_REGIONS, healthRegions: res2.data });
+      })
+      .catch(() => console.log(`error editing data in DB`));
   }
 
   function addRow(data) {
-    return Promise.all([axios.post(`/admin/health_regions/`,{region_name:data.region_name,region_code:data.region_code,stage_id:data.stage_id}),axios.get(`/admin/health_regions`)])
-    .then(([res1,res2])=> {
-      console.log(`data created sucessfully`);
-      dispatch({type: SET_REGIONS, healthRegions: res2.data});
-    })
-    .catch(()=> console.log(`error creating data in DB`));
-
+    return Promise.all([
+      axios.post(
+        `/admin/health_regions/`,
+        {
+          region_name: data.region_name,
+          region_code: data.region_code,
+          stage_id: data.stage_id,
+        },
+        { withCredentials: true }
+      ),
+      axios.get(`/admin/health_regions`, { withCredentials: true }),
+    ])
+      .then(([res1, res2]) => {
+        console.log(`data created sucessfully`);
+        dispatch({ type: SET_REGIONS, healthRegions: res2.data });
+      })
+      .catch(() => console.log(`error creating data in DB`));
   }
 
   return {
@@ -65,7 +95,6 @@ export default function useHealthData(){
     columns,
     editRow,
     addRow,
-    deleteRow
-  }
+    deleteRow,
+  };
 }
-
